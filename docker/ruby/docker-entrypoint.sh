5bf8f0e88e25101ec8a809793d6af2bcb7f714c9
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-17 21:44:39 +0900

    extract TogoStanza dependency

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-if [[ $1 = "start" ]]; then
-  mkdir -p /app/tmp/sockets /app/tmp/pids
-  rm -f /app/tmp/sockets/* /app/tmp/pids/*
-
+if [[ $1 == "unicorn" ]]; then
   bundle install
   yarn install
-
   rails webpacker:compile
 
   cp -rv /app/public/* /var/www/
 
-  if [ -d /stanza ]; then
-    rm -rf /stanza/dist/stanza /var/www/stanza
-
-    ts build -stanza-base-dir /stanza
-    cp -rv /stanza/dist/stanza /var/www/
-  fi
-
   echo
   echo "start unicorn..."
-
-  unicorn --env production -c config/unicorn.rb
-else
-  exec "$@"
 fi
+
+exec "$@"
