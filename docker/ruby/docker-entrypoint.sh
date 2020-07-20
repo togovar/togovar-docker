a797514feb6b9c157dfb421d9343f7e88aa93dc4
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-20 11:57:57 +0900

    clean tmp on start up

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -7,6 +7,9 @@ if [[ $1 == "unicorn" ]]; then
 
   cp -rv /app/public/* /var/www/
 
+  [[ -d /app/tmp/pids ]] && rm -f /app/tmp/pids/*
+  [[ -d /app/tmp/sockets ]] && rm -f /app/tmp/sockets/*
+
   echo
   echo "start unicorn..."
 fi
