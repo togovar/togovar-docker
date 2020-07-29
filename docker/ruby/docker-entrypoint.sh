a8ea4005fe14f9e19b4b27b8cecfa2868720609b
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2020-07-29 22:06:12 +0900

    fix error that the directory is not writable

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -10,8 +10,8 @@ if [[ $1 == "unicorn" ]]; then
 
   cp -rv /app/public/* /var/www/
 
-  [[ -d /app/tmp/pids ]] && rm -f /app/tmp/pids/*
-  [[ -d /app/tmp/sockets ]] && rm -f /app/tmp/sockets/*
+  mkdir -p /app/tmp/pids && rm -f /app/tmp/pids/*
+  mkdir -p /app/tmp/sockets && rm -f /app/tmp/sockets/*
 
   echo
   echo "start unicorn..."
