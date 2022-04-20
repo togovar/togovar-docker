89e729af8b78ff2e2e8fa00a08efacbf0b899db5
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2022-04-20 12:22:03 +0900

    add command to build stanza to start script

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -4,9 +4,19 @@ if [[ $1 == "start" ]]; then
   bundle install
   npm install
 
-  npm run build
-
+  echo "build frontend" >&2
+  npm run build:prod
   cp -rv /app/dist/* /var/www/
+
+  if [[ -d stanza ]]; then
+    echo
+    echo "build stanza" >&2
+
+    cd stanza
+    npm install
+    npm run build --output-path /var/www/stanza
+    cd -
+  fi
 
   mkdir -p /app/tmp/pids && rm -f /app/tmp/pids/*
   mkdir -p /app/tmp/sockets && rm -f /app/tmp/sockets/*
