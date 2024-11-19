8d7b6f22387815a22c679bc158ef29891b8b9953
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2024-11-19 11:43:34 +0900

    clear old files before building frontend

--- docker/ruby/docker-entrypoint.sh
+++ docker/ruby/docker-entrypoint.sh
@@ -5,14 +5,18 @@ set -e
 mkdir -p /opt/togovar/app/tmp/{pids,sockets}
 
 if [[ $1 == "start" ]]; then
-  rm -rfv /opt/togovar/app/tmp/cache/*
-  rm -fv /opt/togovar/app/tmp/pids/unicorn.pid
+  rm -rf /opt/togovar/app/tmp/cache/*
+  rm -f /opt/togovar/app/tmp/pids/unicorn.pid
 
   echo >&2
   echo "start unicorn..." >&2
 
   bundle exec unicorn -c config/unicorn.rb
 elif [[ $1 == "build" ]]; then
+  rm -rf /tmp/stanza
+  rm -rf /opt/togovar/app/dist
+  rm -rf /var/www/*
+
   if [[ -d /opt/togovar/app/stanza ]]; then
     cd /opt/togovar/app/stanza
 
@@ -35,6 +39,7 @@ elif [[ $1 == "build" ]]; then
   echo >&2
   echo "build frontend" >&2
   npm run build
+
   cp -rv /opt/togovar/app/dist/* /var/www/
 else
   exec "$@"
